bc147101c322a8ffcd68751fe53127a1db7f6c88
rupeshchy10 <rupevilary1010@gmail.com>
2025-01-10 17:38:39 +0545

    enhance login functionality and update header styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { MdOutlineArrowDropDownCircle } from "react-icons/md";
 
-
 const Header = () => {
 	return (
-		<div className="flex w-[100%] items-center justify-between px-6 bg-gradient-to-b from-black">
+		<div className="absolute z-10 flex w-[100vw] items-center justify-between px-6 bg-gradient-to-b from-black">
 			<img className="w-40" src="/cinecore_logo.png" alt="cinecore-logo" />
 			<div className="flex items-center">
-         <MdOutlineArrowDropDownCircle size="24px"/>
-				<h1 className="text-lg font-medium">Nightmare</h1>
+				<MdOutlineArrowDropDownCircle size="24px" color="white"/>
+				<h1 className="text-lg font-medium text-white">Nightmare</h1>
 				<div className="ml-4">
-					<button className="bg-red-800 text-white px-4 py-2">Logout</button>
-					<button className="bg-red-800 text-white px-4 py-2 ml-2">Search Movie</button>
+					<button className="bg-red-800 text-white px-4 py-2">
+						Logout
+					</button>
+					<button className="bg-red-800 text-white px-4 py-2 ml-2">
+						Search Movie
+					</button>
 				</div>
 			</div>
 		</div>
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+	const [isLogin, setIsLogin] = useState(false);
+	const [fullname, setFullname] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const loginHandler = () => {
+		setIsLogin(!isLogin);
+	};
+
+	const getInputData = (e) => {
+		e.preventDefault();
+		console.log(fullname, email, password);
+		setFullname("");
+		setEmail("");
+		setPassword("");
+	};
+
 	return (
 		<div>
 			<Header />
-			<div>
-        
-      </div>
+			<div className="absolute">
+				<img
+					className="w-[100vw] h-[100vh] bg-cover"
+					src="/cover_2.jpg"
+					alt="cover-image"
+				/>
+			</div>
+			<form
+				onSubmit={getInputData}
+				className="flex flex-col rounded-md w-3/12 my-36 p-12 left-0 right-0 mx-auto items-center justify-center absolute bg-black opacity-90"
+			>
+				<h1 className="text-3xl text-white mb-5 font-bold">
+					{isLogin ? "Login" : "Signup"}
+				</h1>
+				<div className="flex flex-col">
+					{!isLogin && (
+						<input
+							value={fullname}
+							onChange={(e) => setFullname(e.target.value)}
+							type="text"
+							placeholder="Full Name"
+							className=" p-3 my-2 rounded-sm bg-gray-800 text-white"
+						/>
+					)}
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						type="email"
+						placeholder="Email"
+						className=" p-3 my-2 rounded-sm bg-gray-800 text-white"
+					/>
+					<input
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						type="password"
+						placeholder="Password"
+						className=" p-3 my-2 rounded-sm bg-gray-800 text-white"
+					/>
+					<button className="bg-red-600 mt-5 p-3 text-white rounded-sm font-medium">
+						{isLogin ? "Login" : "Signup"}
+					</button>
+					<p className="text-white mt-2">
+						{isLogin ? "New to CineCore?" : "Already have an account?"}
+						<span
+							onClick={loginHandler}
+							className="ml-1 text-blue-900 font-medium cursor-pointer"
+						>
+							{isLogin ? "Signup" : "Login"}
+						</span>
+					</p>
+				</div>
+			</form>
 		</div>
 	);
 };
